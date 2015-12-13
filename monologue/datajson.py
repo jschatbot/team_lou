d4b3ec9e5be70e29d08c635f856611807b71e2ff
Goryudyuma <umaretekyoumade@gmail.com>
2015-12-13 11:40:46 +0900

    時間取得を一箇所に

--- monologue/datajson.py
+++ monologue/datajson.py
@@ -9,24 +9,18 @@ f = open(file)
 data = json.load(f)
 f.close()
 
-#if str(time.localtime().tm_sec) not in data["begin"]:
-#    sys.exit()
+nowtime = str(time.localtime().tm_sec)
 
-# デプロイ前にsecをhourに変えること
-#str = data["begin"][str(time.localtime().tm_sec)]
+#この一文をデプロイ前に消すこと
+nowtime = "15"
 
-#"0"をデプロイ前にstr(time.localtime().tm_hour)に変えること
-tweetbegin = [m for m in data["begin"]["0"]]
-tweet = random.choice(tweetbegin)
+if nowtime not in data["begin"]:
+    sys.exit()
 
-#if str(time.localtime().tm_hour) in data["end"]:
-#    str += data["end"][str(time.localtime().tm_hour)]
+tweet = random.choice(data["begin"][nowtime])
 
-#"0"をデプロイ前にstr(time.localtime().tm_hour)に変えること
-if "0" in data["end"]:
-    tweetebd = [m for m in data["end"]["0"]]
-    tweet += random.choice(data["end"]["0"])
-    #tweet += data["end"]["0"]
+if nowtime in data["end"]:
+    tweet += random.choice(data["end"][nowtime])
 
 memo = ""
 while memo != tweet:
